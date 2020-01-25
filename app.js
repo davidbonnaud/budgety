20b6b1f9f4c1c5448fe3f6faff8b63aab2e92ec2
David Bonnaud <david@davidbonnaud.com>
2020-01-25 02:50:39 -0800

    built out some event listeners to check for enter press and submit button click

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
+  // some code
 
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
 
-  //some code
+  // some code
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  var ctrlAddItem = function() {
+    // 1. get field input data
+    
+    // 2. add item to the budget controller
 
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+    // 3. add item to the ui
+
+    // 4. calculate budget
+
+    // 5. Display the budget on the ui
+
+    console.log("It works.");
   }
+
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    ctrlAddItem();
+  });
+
+  document.addEventListener('keypress', function(event) {
+
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+
+  });
 
 })(budgetController, UIController);
