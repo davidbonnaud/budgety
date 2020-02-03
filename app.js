d302348029f7f3cd7ac61b2be2ec7b73814c262d
David Bonnaud <david@davidbonnaud.com>
2020-02-02 21:25:43 -0800

    added function to clear fields after data entry in UI controller

--- app.js
+++ app.js
@@ -98,6 +98,19 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -135,9 +148,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     // 3. add item to the ui
     UICtrl.addListItem(newItem, input.type);
-    // 4. calculate budget
 
-    // 5. Display the budget on the ui
+    // 4. clear fields
+    UICtrl.clearFields();
+
+    // 5. calculate budget
+
+    // 6. Display the budget on the ui
 
   };
 
