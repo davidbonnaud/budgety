1d6502f7c232dda105ca56be64e87ea86d1021a1
David Bonnaud <david@davidbonnaud.com>
2020-02-22 07:15:24 -0800

    added delete item method in which entire parent container of the item listing is selected, and the unique ID is pulled for later use

--- app.js
+++ app.js
@@ -107,7 +107,8 @@ var UIController = (function() {
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    container: '.container'
   }
 
   return {
@@ -125,10 +126,10 @@ var UIController = (function() {
 
       if (type === 'inc'){
         element = DOMstrings.incomeContainer;
-        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
       } else if (type === 'exp') {
         element = DOMstrings.expensesContainer;
-        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
       }
       
       // repleace placeholder text with some actual data
@@ -179,9 +180,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   var setupEventListeners = function() {
     var DOM = UICtrl.getDOMstrings();
 
-    document.querySelector(DOM.inputButton).addEventListener('click', function() {
-      ctrlAddItem();
-    });
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
   
     document.addEventListener('keypress', function(event) {
   
@@ -190,6 +189,9 @@ var controller = (function(budgetCtrl, UICtrl) {
       }
   
     });
+
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
   };
 
   var updateBudget = function() {
@@ -223,6 +225,27 @@ var controller = (function(budgetCtrl, UICtrl) {
       updateBudget();
     }
     
+  };
+
+  var ctrlDeleteItem = function(event) {
+    var itemID, splitID, type, ID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      splitID = itemID.split('-');
+      type = splitID[0];
+      ID = splitID[1];
+
+      // 1. delete the item from the data structure
+      
+
+      // 2. delete the item from the UI
+
+
+      // 3. update and show the new budget
+
+    }
   };
 
   return {
